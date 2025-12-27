47f75769e9f0a5b9ec3767d1a878f31a5c4b00a5
Lazar Popovic <lazar@gmail.com>
2025-12-27 16:59:36 +0100

    Initial sales analysis script

---
+++ sales_analysis.py
@@ -0,0 +1,8 @@
+sales = [120, 340, 560, 230, 410]
+
+total_sales = sum(sales)
+average_sales = total_sales / len(sales)
+
+print("Sales data:", sales)
+print("Total sales:", total_sales)
+print("Average sales:", average_sales)
